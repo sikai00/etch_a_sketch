810076afe8e851ec86e7ad4fc824ab6d7dd303dd
Si Kai <sikai4111@gmail.com>
2022-05-25 22:35:52 +0800

    Tidy up

--- main.js
+++ main.js
@@ -1,6 +1,41 @@
 let primaryColor = '#000000';
 let rainbowMode = false;
 
+// Main program
+function main() {
+  createGrid(getGridLength());
+  setUpControls();
+}
+
+main();
+
+// Main program helper functions
+// Set up event listeners for the controls, including the grid erase slider
+function setUpControls() {
+  // Color picker
+  document.querySelector('.color-picker').addEventListener('change', function(e) {
+    primaryColor = e.target.value;
+  });
+
+  // Rainbow mode button
+  document.querySelector('.rainbow-mode-btn').addEventListener('click', toggleRainbowMode);
+
+  // Grid length slider
+  document.querySelector('.grid-length-slider').addEventListener('mouseup', function(e) {
+    clearGrid();
+    createGrid(e.target.value);
+    const gridLength = getGridLength();
+    document.querySelector('.grid-length').textContent = gridLength + " x " + gridLength;
+  })
+  const gridLength = getGridLength();
+  document.querySelector('.grid-length').textContent = gridLength + " x " + gridLength;
+
+  // Grid erase slider
+  document.querySelector('.grid-erase-slider').addEventListener('mousemove', e => gridEraser(e));
+}
+
+
+// Sketchboard related functions
 function createGrid(length) {
   const container = document.querySelector('.grid-container');
   container.style.gridTemplateRows = `repeat(${length}, 1fr)`;
@@ -10,18 +45,14 @@ function createGrid(length) {
       const element = document.createElement('div');
       element.classList.add('grid-element');
       element.classList.add(`col-${j}`);
-      element.addEventListener('mousedown', e => onClickAndDrag(e, primaryColor));
-      element.addEventListener('mouseenter', e => onClickAndDrag(e, primaryColor));
+      element.addEventListener('mousedown', e => sketch(e, primaryColor));
+      element.addEventListener('mouseenter', e => sketch(e, primaryColor));
       container.appendChild(element);
     }
   }
 }
 
-function toggleRainbowMode() {
-  rainbowMode = !rainbowMode;
-}
-
-function onClickAndDrag(e, color) {
+function sketch(e, color) {
   if (e.buttons != 0) {
     if (!rainbowMode) {
       e.target.style.backgroundColor = color;
@@ -40,28 +71,24 @@ function clearGrid() {
   }
 }
 
-function getGridLength() {
-  return document.querySelector('.grid-length-slider').value;
-}
-
 function gridEraser(e) {
   if (e.buttons != 0) {
     const eraseSlider = document.querySelector('.grid-erase-slider');
-    const gridContainer = document.querySelector('.grid-container');
-
     const step = eraseSlider.getAttribute('max') / getGridLength();
     const currCol = Math.round(eraseSlider.value / step) - 1;
-    gridContainer.querySelectorAll(`.col-${currCol}`).forEach(x => x.style.backgroundColor = 'white');
+
+    const gridContainer = document.querySelector('.grid-container');
+    gridContainer.querySelectorAll(`.col-${currCol}`).forEach(
+      element => element.style.backgroundColor = null
+    );
   }
 }
 
-createGrid(getGridLength());
-document.querySelector('.color-picker').addEventListener("change", function(e) {
-  primaryColor = e.target.value;
-});
-document.querySelector('.rainbow-mode-btn').addEventListener('click', toggleRainbowMode);
-document.querySelector('.grid-length-slider').addEventListener('mouseup', function(e) {
-  clearGrid();
-  createGrid(e.target.value);
-})
-document.querySelector('.grid-erase-slider').addEventListener('mousemove', e => gridEraser(e));
+// Miscellaneous functions
+function toggleRainbowMode() {
+  rainbowMode = !rainbowMode;
+}
+
+function getGridLength() {
+  return document.querySelector('.grid-length-slider').value;
+}
