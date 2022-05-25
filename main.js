a44c4c0d3b610f185eb99e6b279b8dbbbd6e9785
Si Kai <sikai4111@gmail.com>
2022-05-25 19:45:51 +0800

    Add additional controls

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+let primaryColor = '#000000';
+let rainbowMode = false;
+
 function createGrid(length) {
   const container = document.querySelector('.grid-container');
   container.style.gridTemplateRows = `repeat(${length}, 1fr)`;
@@ -5,20 +8,42 @@ function createGrid(length) {
   for (let i = 0; i < length * length; i++) {
     const element = document.createElement('div');
     element.classList.add('grid-element');
-    element.addEventListener('mousedown', e => onClick(e, 'blue'));
-    element.addEventListener('mouseenter', e => onClickAndDrag(e, 'blue'));
+    element.addEventListener('mousedown', e => onClickAndDrag(e, primaryColor));
+    element.addEventListener('mouseenter', e => onClickAndDrag(e, primaryColor));
     container.appendChild(element);
   }
 }
 
+function toggleRainbowMode() {
+  rainbowMode = !rainbowMode;
+}
+
 function onClickAndDrag(e, color) {
   if (e.buttons != 0) {
-    e.target.style.backgroundColor = color;
+    if (!rainbowMode) {
+      e.target.style.backgroundColor = color;
+    } else {
+      let randomColor = Math.floor(Math.random()*16777215).toString(16);
+      console.log(randomColor);
+      e.target.style.backgroundColor = "#" + randomColor;
+    }
   }
 }
 
-function onClick(e, color) {
-  e.target.style.backgroundColor = color;
+function clearGrid() {
+  const container = document.querySelector('.grid-container');
+  while (container.firstChild) {
+    container.removeChild(container.lastChild);
+  }
 }
 
-createGrid(10);
+
+createGrid(9);
+document.querySelector('.color-picker').addEventListener("change", function(e) {
+  primaryColor = e.target.value;
+});
+document.querySelector('.rainbow-mode-btn').addEventListener('click', toggleRainbowMode);
+document.querySelector('.grid-length-slider').addEventListener('mouseup', function(e) {
+  clearGrid();
+  createGrid(e.target.value);
+})
